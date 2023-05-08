8cb441c15656be6e1cf212d23570f4cbb1da510f
xXWhyBroXx <104370028+xXWhyBroXx@users.noreply.github.com>
2023-05-08 14:03:52 -0400

    Create dfbdfbxdb.c

---
+++ dfbdfbxdb.c
@@ -0,0 +1,47 @@
+	#pragma config(Sensor, in1,    lineFollowerRIGHT,   sensorLineFollower)
+#pragma config(Sensor, in2,    lineFollowerCENTER,  sensorLineFollower)
+#pragma config(Sensor, in3,    lineFollowerLEFT,    sensorLineFollower)
+#pragma config(Motor,  port2,           rightMotor,    tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port3,           leftMotor,     tmotorNormal, openLoop)
+
+task main()
+{
+  wait1Msec(2000);          // The program waits for 2000 milliseconds before continuing.
+
+  int threshold = 2400;      /* found by taking a reading on both DARK and LIGHT    */
+                            /* surfaces, adding them together, then dividing by 2. */
+  while(true)
+  {
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
+    displayLCDCenteredString(0, "LEFT  CNTR  RGHT");        //  Display   |
+    displayLCDPos(1,0);                                     //  Sensor    |
+    displayNextLCDNumber(SensorValue(lineFollowerLEFT));    //  Readings  |
+    displayLCDPos(1,6);                                     //  to LCD.   |
+    displayNextLCDNumber(SensorValue(lineFollowerCENTER));  //            |
+    displayLCDPos(1,12);                                    //  L  C  R   |
+    displayNextLCDNumber(SensorValue(lineFollowerRIGHT));   //  x  x  x   |
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
+
+    // RIGHT sensor sees dark:
+    if(SensorValue(lineFollowerRIGHT) > threshold)
+    {
+      // counter-steer right:
+      motor[leftMotor]  = 33;
+      motor[rightMotor] = 0;
+    }
+    // CENTER sensor sees dark:
+    if(SensorValue(lineFollowerCENTER) > threshold)
+    {
+      // go straight
+      motor[leftMotor]  = 33;
+      motor[rightMotor] = 33;
+    }
+    // LEFT sensor sees dark:
+    if(SensorValue(lineFollowerLEFT) > threshold)
+    {
+      // counter-steer left:
+      motor[leftMotor]  = 0;
+      motor[rightMotor] = 33;
+    }
+  }
+}
